6a00f70d69cc1c97ffb8d5c555e7f2fae3c6a0b5
Tom King <mobilemind@pobox.com>
2011-12-28 10:29:49 -0800

    fix URI encoding and more readable try/throw/catch

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,37 +3,44 @@
 // listener to dynamically position page for initial or return-trip
 window.addEventListener('load',
 	function() {
-		var q = location.search, p = '', skp;
-		if ('' != q) {
+		if (location.search) {
 				// reload form UI from query string
 				try {
-					p = decodeURIComponent(q.match(/^\?javascript:var( |%20)s(=|%3D)('|%27)(.*?)('|%27),f(=|%3D)/)[4]);
-					document.title = 'Paste ' + p;
-					document.getElementById('pStr').value = p;
-					// if generic pastelet maker "Skip...?" exists, set 'skip confirm' accordingly
-					skp = document.getElementById("skp");
-					if (null != skp) {
-						if (q.indexOf(';if(confirm(') < 0) skp.checked = 'on';
-					}
-					// put a more human-readable, but compatible, version of bookmarklet in textarea
-					document.getElementById('bmrk').textContent = q.substring(1).replace(/\%27/g,"'");
-					// if not iPhone/iPad unhide 'Pastelet as link' and set anchor tag
-					if (!(-1 != navigator.userAgent.indexOf('Safari') && -1 != navigator.userAgent.indexOf('Mobile'))) {
-						var pal = document.getElementById('pal'), pl = document.getElementById('pl');
-						if (null != pal && null != pl) {
-							pal.setAttribute("style", "display:inline;padding:7px;height:auto");
-							pl.setAttribute('href', q.substring(1));
-							pl.setAttribute('title', document.title);
-							pl.innerHTML = document.title;
+					var q = location.search, m = [];
+					q = decodeURIComponent(q.substr(1));
+					m = q.match(/^javascript:s='(.*?)';,f'/);
+					if (!m) throw 'No match in ' + q;
+					else {
+						var p = m[1], skp;
+						if (!p) throw 'Empty or invalid string to paste- ' + p;
+						else {
+							document.title = 'Paste ' + p;
+							document.getElementById('pStr').value = p;
+							// if generic pastelet maker "Skip...?" exists, set 'skip confirm' accordingly
+							skp = document.getElementById("skp");
+							if (skp && q.indexOf(';if(confirm(') < 0) skp.checked = 'on';
+							// put a more human-readable, but URI encoded, version of bookmarklet in textarea
+							document.getElementById('bmrk').textContent = encodeURI(q);
+							// if not iPhone/iPad unhide 'Pastelet as link' and set anchor tag
+							if (!(-1 != navigator.userAgent.indexOf('Safari') && -1 != navigator.userAgent.indexOf('Mobile'))) {
+								var pal = document.getElementById('pal'), pl = document.getElementById('pl');
+								if (pal && pl) {
+									pal.setAttribute("style", "display:inline;padding:7px;height:auto");
+									pl.setAttribute('href', document.getElementById('bmrk').textContent);
+									pl.setAttribute('title', document.title);
+									pl.innerHTML = document.title;
+								}
+							}
+							// unhide remaining steps
+							window.scrollTo(0,78+document.getElementById('pltMkr').scrollHeight);
 						}
 					}
-		 			// unhide remaining steps
-					window.scrollTo(0,78+document.getElementById('pltMkr').scrollHeight)
 				}
 				catch (e) {
+					console.log('error: ' + e);
 					alert("Unable to decode pastelet.\r\nForm will be reset.");
 					location.href=location.href.substring(0,location.href.indexOf('?'));
-					return
+					return;
 				}
 		}
 		// hide address bar
@@ -42,9 +49,9 @@ window.addEventListener('load',
 ,true);
 // reload page with new bookmarklet appended
 function loadpg(p) {
-	if ('' != p) {
-	  document.getElementById('bmrk').textContent = p;
-	  location.href = location.protocol + '//' + location.host + location.port + location.pathname + '?' + p
+	if (p) {
+	  document.getElementById('bmrk').textContent = 'javascript:' + encodeURI(p);
+	  location.href = location.protocol + '//' + location.host + location.port + location.pathname + '?' + '?javascript:' + encodeURIComponent(p);
 	}
 }
 // -->
