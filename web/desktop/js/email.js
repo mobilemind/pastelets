dda3befb00a212ed9110f1979d392fad934261de
Tom King <mobilemind@pobox.com>
2017-06-06 00:33:28 -0700

    web/**: republish new version

--- web/desktop/js/email.js
+++ web/desktop/js/email.js
@@ -2,54 +2,56 @@
 // unique version of pastelet for pasting into email/login input field WITHOUT confirmation
 //
 
-//	pastelet- create bookmarklet for Mobile Safari to 'paste' provided text into HTML form text field
+//  pastelet- create bookmarklet for Mobile Safari to 'paste' provided text into HTML form text field
 //
-//	Copyright (c) _MmCOPYRIGHT_ Tom King. All rights reserved.
+//  Copyright (c) _MmCOPYRIGHT_ Tom King. All rights reserved.
 //
 // Comments- Generated bookmarklet is carefully constructed.
-// *	The 'paste string' variable ('n') is defined first to make it more visible and ease editing the bookmark itself.
-// *	The text entry types for HTML5 are checked, except search. Search is skipped as a convenience (fixed text for a search is rare)
-// *	Bookmarklet code is optimized for size (ie, string length of all the code), given goal above.
+// *  The 'paste string' variable ('n') is defined first to make it more visible and ease editing the bookmark itself.
+// *  The text entry types for HTML5 are checked, except search. Search is skipped as a convenience (fixed text for a search is rare)
+// *  Bookmarklet code is optimized for size (ie, string length of all the code), given goal above.
 // ** it wasn't written as an anonymous function, because the 'function(){..}' declaration adds characters
 // * void(0) is included for cross-browser compatibility, a non-null return value causes some browsers to navigate
 //
 
 function pastelet(u) {
-	if (u) {
-		// strip leading/trailing spaces to help w/iOS 5 shortcut text & pastelet
-		return "var s='" + u.match(/^\s*(.\S*?)\s*$/)[1] + "',f=document.getElementsByTagName('input'),x=-1,b=x,j=0,l=f.length,k,n,t;for(;j<l;j++){k=f[j];n=k.id+k.name+k.title;t=k.type;if(t in{email:1,login:1}||(t!='hidden'&&(n.indexOf('email')>-1||n.indexOf('ogin')>0||n.indexOf('user')>-1)))x=j,break;if(-1==b&&'text'==t&&n.indexOf('earch')<0)b=j}if(-1==x)x=b;if(-1!=x){f[x].focus();f[x].value=s;f[x].blur()};void'_MmVERSION_'";
-	}
+  if (u) {
+    // strip leading/trailing spaces to help w/iOS 5 shortcut text & pastelet
+    return "var s='" + u.match(/^\s*(.\S*?)\s*$/)[1] +
+      "',f=document.getElementsByTagName('input'),x=-1,b=x,j=0,l=f.length,k,n,t;for(;j<l;j++){k=f[j];n=k.id+k.name+k.title;t=k.type;if(t in{email:1,login:1}||(t!='hidden'&&(n.indexOf('email')>-1||n.indexOf('ogin')>0||n.indexOf('user')>-1)))x=j,break;if(-1==b&&'text'==t&&n.indexOf('earch')<0)b=j}if(-1==x)x=b;if(-1!=x){f[x].focus();f[x].value=s;f[x].blur()};void'_MmVERSION_'";
+  }
+  return null;
 }
 
 //
 // annotated version of code for generated bookmark
 //
 // var s = 'your text', // string to 'paste'
-//	// all potential input fields
-//	f = document.getElementsByTagName('input'),
-//	x = -1,
-//	b = x,
-//	j = 0,
-//	l = f.length,
-//	k, n, t;
-//	// loop through all inputs
-//	for (; j < l; j++) {
-//		k = f[j];
-//		n = k.id + k.name + k.title; // join id, name, & title to easily check all at once
-//		t = k.type;
-//		// store index and break when type, id or name matches 'email' or 'ogin' (login/Login) or 'user' (user/userid/username)
-//		if (t in {email:1,login:1} || (t != 'hidden' && (n.indexOf('email') > -1 || n.indexOf('ogin') > 0 || n.indexOf('user') >-1))) x = j , break;
-//		// set fallback to first 'non-search' text input (uses 'earch' to test for Search or search)
-//		if (-1 == b && 'text' == t && n.indexOf('earch') < 0) b = j;
-//	}
-//	// no match? use fallback
-//	if(-1 == x) x = b;
-//	if (-1 != x) {
-//		// set focus and value to match
-//		f[x].focus();
-//		f[x].value = s;
-//		// blur to trigger a change event (helps some frameworks)
-//		f[x].blur()
-//	}
-//	else alert('Email field not found');
-//	void '_MmVERSION_' // return void so browser doesn't navigate, etc.
+//  // all potential input fields
+//  f = document.getElementsByTagName('input'),
+//  x = -1,
+//  b = x,
+//  j = 0,
+//  l = f.length,
+//  k, n, t;
+//  // loop through all inputs
+//  for (; j < l; j++) {
+//    k = f[j];
+//    n = k.id + k.name + k.title; // join id, name, & title to easily check all at once
+//    t = k.type;
+//    // store index and break when type, id or name matches 'email' or 'ogin' (login/Login) or 'user' (user/userid/username)
+//    if (t in {email:1,login:1} || (t != 'hidden' && (n.indexOf('email') > -1 || n.indexOf('ogin') > 0 || n.indexOf('user') >-1))) x = j , break;
+//    // set fallback to first 'non-search' text input (uses 'earch' to test for Search or search)
+//    if (-1 == b && 'text' == t && n.indexOf('earch') < 0) b = j;
+//  }
+//  // no match? use fallback
+//  if(-1 == x) x = b;
+//  if (-1 != x) {
+//    // set focus and value to match
+//    f[x].focus();
+//    f[x].value = s;
+//    // blur to trigger a change event (helps some frameworks)
+//    f[x].blur()
+//  }
+//  else alert('Email field not found');
+//  void '_MmVERSION_' // return void so browser doesn't navigate, etc.
--- web/desktop/js/loader.js
+++ web/desktop/js/loader.js
@@ -1,58 +1,61 @@
 // loader _MmVERSION_, _MmBUILDDATE_
 // listener to dynamically position page for initial or return-trip
 window.addEventListener('load',
-	function() {
-		if (location.search) {
-				// reload form UI from query string
-				try {
-					var q = location.search, m = [];
-					q = decodeURIComponent(q.substr(1));
-					m = q.match(/^javascript:var s='(.*?)',f/);
-					if (!m) {
-						throw {name: 'NoMatchInURL', message: 'No match in ' + q};
-					}
-					else {
-						var p = m[1], skp = null;
-						if (!p) {
-							throw {name: "EmptyString", message: 'Empty or invalid string to paste- ' + p};
-						}
-						else {
-							document.title = 'Paste ' + p;
-							document.getElementById('pStr').value = p;
-							// if generic pastelet maker "Skip...?" exists, set 'skip confirm' accordingly
-							skp = document.getElementById("skp");
-							if (skp && q.indexOf(';if(confirm(') < 0) {
-								skp.checked = 'on';
-							}
-							// put a more human-readable, but URI encoded, version of bookmarklet in textarea
-							document.getElementById('bmrk').textContent = encodeURI(q);
-							// if not iPhone/iPad unhide 'Pastelet as link' and set anchor tag
-							if (!(-1 !== navigator.userAgent.indexOf('Safari') && -1 !== navigator.userAgent.indexOf('Mobile'))) {
-								var pal = document.getElementById('pal'), pl = document.getElementById('pl');
-								if (pal && pl) {
-									pal.style = "display:inline;padding:7px;height:auto";
-									pl.href = document.getElementById('bmrk').textContent;
-									pl.title = document.title;
-									pl.innerHTML = document.title;
-								}
-							}
-							// unhide remaining steps
-							window.scrollTo(0,78+document.getElementById('pltMkr').scrollHeight);
-						}
-					}
-				}
-				catch (e) {
-					console.log('error: ' + e);
-					alert("Unable to decode pastelet.\nForm will be reset.");
-					location.replace('//' + location.host + location.port + location.pathname);
-				}
-		}
-		return;
-	},true);
+  function() {
+    if (location.search) {
+      // reload form UI from query string
+      try {
+        let m = [];
+        const q = decodeURIComponent(location.search.substr(1));
+        m = q.match(/^javascript:var s='(.*?)',f/);
+        if (!m) {
+          throw {
+            "message": "No match in " + location.search,
+            "name": "NoMatchInURL"
+          };
+        } else {
+          const p = m[1];
+          let skp = null;
+          if (!p) {
+            throw {
+              "message": 'Empty or invalid string to paste- ' + p,
+              "name": "EmptyString"
+            };
+          } else {
+            document.title = 'Paste ' + p;
+            document.getElementById('pStr').value = p;
+            // if generic pastelet maker "Skip...?" exists, set 'skip confirm' accordingly
+            skp = document.getElementById("skp");
+            if (skp && q.indexOf(';if(confirm(') < 0) {
+              skp.checked = 'on';
+            }
+            // put a more human-readable, but URI encoded, version of bookmarklet in textarea
+            document.getElementById('bmrk').textContent = encodeURI(q);
+            // if not iPhone/iPad unhide 'Pastelet as link' and set anchor tag
+            if (!(-1 !== navigator.userAgent.indexOf('Safari') && -1 !== navigator.userAgent.indexOf('Mobile'))) {
+              const pal = document.getElementById('pal'), pl = document.getElementById('pl');
+              if (pal && pl) {
+                pal.style = "display:inline;padding:7px;height:auto";
+                pl.href = document.getElementById('bmrk').textContent;
+                pl.title = document.title;
+                pl.innerHTML = document.title;
+              }
+            }
+            // unhide remaining steps
+            window.scrollTo(0, 78 + document.getElementById('pltMkr').scrollHeight);
+          }
+        }
+      } catch (e) {
+        console.log('error: ' + e);
+        alert("Unable to decode pastelet.\nForm will be reset.");
+        location.replace('//' + location.host + location.port + location.pathname);
+      }
+    }
+  }, true);
 // reload page with new bookmarklet appended
 function loadpg(p) {
-	if (p) {
-		document.getElementById('bmrk').textContent = 'javascript:' + encodeURI(p);
-		location.replace('//' + location.host + location.port + location.pathname + '?javascript:' + encodeURIComponent(p));
-	}
+  if (p) {
+    document.getElementById('bmrk').textContent = 'javascript:' + encodeURI(p);
+    location.replace('//' + location.host + location.port + location.pathname + '?javascript:' + encodeURIComponent(p));
+  }
 }
